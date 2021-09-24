372d8ac2fa18a69958630beeccf645ba66f1d0be
Khang Vuong <khangdieuvuong@gmail.com>
2021-09-23 19:21:43 -0600

    implement server.py

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-CMPUT404-assignment-webserver
+# CMPUT404-assignment-webserver
+
+How to run
+
+Run runner.sh to run freetests.py and not-free-tests.py. If runner.sh gives error run `sed -i -e 's/\r$//' runner.sh`, source: https://stackoverflow.com/a/29747593
+
 =============================
 
 CMPUT404-assignment-webserver
@@ -7,20 +12,19 @@ See requirements.org (plain-text) for a description of the project.
 
 Make a simple webserver.
 
-Contributors / Licensing
-========================
+# Contributors / Licensing
 
-Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle.
+Generally everything is LICENSE'D under the MIT license by Khang Vuong
 
 server.py contains contributions from:
 
-* Abram Hindle
-* Eddie Antonio Santos
-* Jackson Z Chang
-* Mandy Meindersma 
+- Abram Hindle
+- Eddie Antonio Santos
+- Jackson Z Chang
+- Mandy Meindersma
+- Khang Vuong
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
-
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #  coding: utf-8 
 import socketserver
-
+from os.path import abspath, splitext
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,13 +26,74 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+extensions_map = {
+    ".manifest": "text/cache-manifest",
+	".html": "text/html",
+    ".png": "image/png",
+	".jpg": "image/jpg",
+	".svg":	"image/svg+xml",
+	".css":	"text/css",
+	".js":	"application/x-javascript",
+	"": "application/octet-stream", # Default
+}
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        raw_data = self.request.recv(1024).strip()
+        print(raw_data)
+
+        if raw_data == b'':
+            self.request.sendall(bytearray(f"HTTP/1.1 404 Not Found\r\nConnection: close \r\n\r\n", "utf-8"))
+        else:
+            self.parse_request(raw_data)
+
+            if self.data["method"] == "GET":
+                self.handle_get()
+            else:
+                self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\n", "utf-8"))
+        
+        self.request.close()
+
+    def parse_request(self, raw_data):
+        decoded = raw_data.decode()
+        headers = decoded.split('\r\n\r\n')
+        headers = headers[0].split('\r\n')
+        method, path, _ = headers.pop(0).split(" ")
+        data = {
+            "method": method,
+            "path": path
+        }
+        for header in headers:
+            key, val = header.split(": ", 1)
+            data[key.lower()] = val
+
+        self.data = data
+
+    def handle_get(self):
+        host = self.data["host"]
+        path_length = len(self.data["path"])
+        temp_path = self.data["path"]
+        path = f"{temp_path}index.html" if temp_path[path_length - 1] == "/" else temp_path
+        file_path = f"./www{path}"
+
+        # validate path is within ./wwww
+        abs_path = abspath(file_path)
+        if not abs_path.startswith(abspath("./www")):
+            self.request.sendall(bytearray(f"HTTP/1.1 404 Not Found\r\nConnection: close \r\n\r\n", "utf-8"))
+            return
+
+        try:
+            f = open(file_path)
+            _, file_extension = splitext(path)
+            self.request.sendall(bytearray(f"HTTP/1.1 200\r\nContent-Type: {extensions_map[file_extension]}\r\nConnection: close \r\n\r\n {f.read()}", "utf-8"))
+        except IsADirectoryError:
+            # redirect
+            self.request.sendall(bytearray(f"HTTP/1.1 301 Moved\r\nLocation: http://{host}{path}/\r\nConnection: close \r\n\r\n", "utf-8"))
+        except:
+            self.request.sendall(bytearray(f"HTTP/1.1 404 Not Found\r\nConnection: close \r\n\r\n", "utf-8"))
+
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
